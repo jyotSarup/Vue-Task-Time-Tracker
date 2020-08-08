cac51dbc3f9791d05cee776b4577e09f23a76c7e
Jyot Sarup <52942229+jyotSarup@users.noreply.github.com>
2020-08-07 17:50:02 -0700

    Record task time component with local storage feature

--- src/components/TaskActivity.vue
+++ src/components/TaskActivity.vue
@@ -1,25 +1,85 @@
 <template>
-  <div>
-    <h3>Task Activity</h3>
-    <div class="recordActivityDiv">
-        <q-btn class="timerButton" icon="timer" label="Start Activity" stack glossy color="green" />
-        <q-btn class="timerButton" icon="timer_off" label="Stop Activity" stack glossy color="red" />
+  <div class="recordActivityDiv">
+    <h4 class="recordActivityHeading">Task Activity</h4>
+    <div >
+      <div v-if="!activityStarted" class="timerButtonDiv">
+        <q-btn
+          icon="timer"
+          label="Start Activity"
+          stack
+          glossy
+          color="green"
+          @click="startActivity"
+        />
+      </div>
+      <div v-else class="timerButtonDiv">
+        
+        <q-btn
+          icon="timer_off"
+          label="Stop Activity"
+          stack
+          glossy
+          color="red"
+          @click="stopActivity"
+        />
+        <h6>Activity Started at : {{startedActivityTime.substr(0,25)}}</h6>
+      </div>
     </div>
-  </div>
-</template>;
+  </div> </template
+>;
 
 <script>
+import { mapActions, mapState } from 'vuex';
+import store from '../store';
 
 export default {
-  
-}
+  data() {
+    return {
+      activityStarted: false,
+      startedActivityTime: ""
+    };
+  },
+  methods: {
+    startActivity() {
+      this.startedActivityTime = (new Date()).toString();
+      this.activityStarted = true;
+      localStorage.startedActivityTime = this.startedActivityTime;
+      localStorage.activityStarted = this.activityStarted;
+    },
+    stopActivity() {
+      var stoppedTime = (new Date()).toString();
+      this.startedActivityTime = ""
+      this.activityStarted = false;
+      
+      localStorage.startedActivityTime = this.startedActivityTime;
+      localStorage.activityStarted = this.activityStarted;
+    }
+  },
+  mounted() {
+    if (localStorage.activityStarted && localStorage.startedActivityTime!="") {
+      this.activityStarted = localStorage.activityStarted;
+      this.startedActivityTime = localStorage.startedActivityTime;
+    }
+  },
+  computed: {
+    ...mapState({
+      projects: state => state.projects
+    })
+  }
+};
 </script>
 
 <style scoped>
-  .timerButton{
-    margin: auto;
-  }
-  .recordActivityDiv{
-    margin:auto;
-  }
+.timerButtonDiv {
+ text-align: center;
+ padding:1em;
+}
+.recordActivityDiv {
+  width:100%;
+  margin: auto;
+}
+.recordActivityHeading{
+  margin: auto;
+  text-align: center;
+}
 </style>
